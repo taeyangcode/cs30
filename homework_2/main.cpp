9686e714869e5fff96d0b7dc7fbe73cbdbbc4485
taeyangcode <coreymostero@gmail.com>
2022-10-17 01:56:01 -0700

    homework_2

---
+++ homework_2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "smart_ptr.h"
+
+int main() {
+    return 0;
+}
---
+++ homework_2/smart_ptr.h
@@ -0,0 +1,92 @@
+#ifndef SMART_PTR_H_
+#define SMART_PTR_H_
+
+template <typename T>
+class smart_ptr {
+   private:
+    T* _data;
+    unsigned int* _referenceCount;
+
+   public:
+    ~smart_ptr() {
+        if (this->_referenceCount != nullptr && --*this->_referenceCount == 0) {
+            delete this->_data;
+            delete this->_referenceCount;
+        }
+    }
+
+    smart_ptr() {
+        this->_data = nullptr;
+        this->_referenceCount = nullptr;
+    }
+
+    explicit smart_ptr(T* raw_ptr) {
+        this->_data = raw_ptr;
+        this->_referenceCount = new unsigned int(1);
+    }
+
+    smart_ptr(const smart_ptr& rhs) {
+        this->_data = rhs._data;
+        this->_referenceCount = rhs._referenceCount;
+        ++*this->_referenceCount;
+    }
+
+    smart_ptr(smart_ptr&& rhs) {
+        this->_data = rhs._data;
+        this->_referenceCount = rhs._referenceCount;
+
+        rhs._data = nullptr;
+        rhs._referenceCount = nullptr;
+    }
+
+    smart_ptr& operator=(const smart_ptr& rhs) {
+        if (this != &rhs) {
+            this->_data = rhs._data;
+            this->_referenceCount = rhs._referenceCount;
+            ++*this->_referenceCount;
+        }
+        return *this;
+    }
+
+    smart_ptr& operator=(smart_ptr&& rhs) {
+        if (this != &rhs) {
+            this->_data = rhs._data;
+            this->_referenceCount = rhs._referenceCount;
+
+            rhs._data = nullptr;
+            rhs._referenceCount = nullptr;
+        }
+        return *this;
+    }
+
+    bool clone() {
+        if (this == nullptr || *this->_referenceCount == 1) {
+            return false;
+        }
+
+        --*this->_referenceCount;
+        this->_data = new T(*this->_data);
+        this->_referenceCount = new unsigned int(1);
+        return true;
+    }
+
+    int ref_count() const {
+        return (this->_referenceCount == nullptr) ? 0 : *this->_referenceCount;
+    }
+
+    T& operator*() {
+        if (this->_data == nullptr) {
+            throw std::runtime_error("null_ptr_exception");
+        }
+        return *this->_data;
+    }
+
+    T* operator->() {
+        if (this->_data == nullptr) {
+            throw std::runtime_error("null_ptr_exception");
+        }
+        return this->_data;
+    }
+};
+
+#endif
