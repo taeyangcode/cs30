5b0037b2dac5c61909a3c10944d92f962c5af314
taeyangcode <coreymostero@gmail.com>
2022-11-03 12:33:47 -0700

    project_2
    Tests finished for removeBad and anagram

--- project_2/anagram.cpp
+++ project_2/anagram.cpp
@@ -1,50 +1,50 @@
+#include <algorithm>
+#include <cassert>
+#include <csignal>
 #include <fstream>
 #include <iostream>
 #include <istream>
+#include <sstream>
 #include <string>
+#include <unordered_set>
 #include <vector>
+using namespace std;
 
-const int MAXRESULTS = 20;       // Max matches that can be found
-const int MAXDICTWORDS = 30000;  // Max words that can be read in
+const int MAXRESULTS = 20;    // Max matches that can be found
+const int MAXDICTWORDS = 10;  // Max words that can be read in
 
 int loadDictionary(std::istream& dictfile, std::vector<std::string>& dict) {
     int wordsLoaded = 0;
     std::string currentWord;
-    for (dictfile >> currentWord; !dictfile.eof(); ++wordsLoaded, dictfile >> currentWord) {
+    for (dictfile >> currentWord; !dictfile.eof() && wordsLoaded < MAXDICTWORDS; ++wordsLoaded, dictfile >> currentWord) {
         dict.push_back(currentWord);
     }
     return wordsLoaded;
 }
 
-void findPermutations(std::vector<std::string>& permutations, std::string current, unsigned int index, unsigned int size) {
-    if (index + 2 >= size) {
-        for (std::string s : permutations) {
-            if (current == s) {
-                return;
-            }
-        }
-        permutations.push_back(current);
+void findPermutations(std::unordered_set<std::string>& permutations, const std::string& current, unsigned int index, unsigned int size) {
+    if (index + 1 >= size) {
+        permutations.insert(current);
         return;
     }
-    for (int i = index; i < size; ++i) {
-        std::string currentCopy = current;
-        currentCopy[index] = current[i];
-        currentCopy[i] = current[index];
-        findPermutations(permutations, currentCopy, index + 1, size);
+    for (unsigned int i = index; i < size; ++i) {
+        std::string temp = current;
+        temp[index] = current[i];
+        temp[i] = current[index];
+        findPermutations(permutations, temp, index + 1, size);
     }
 }
 
 int permute(std::string word, std::vector<std::string>& dict, std::vector<std::string>& results) {
     int matches = 0;
-    std::vector<std::string> permutations;
+    std::unordered_set<std::string> permutations;
     findPermutations(permutations, word, 0, word.size());
 
-    for (std::string permutation : permutations) {
-        for (auto dictIndex = dict.begin(); dictIndex != dict.end(); ++dictIndex) {
-            if (permutation == *dictIndex) {
-                results.push_back(permutation);
-                ++matches;
-                break;
+    for (const std::string& word : dict) {
+        if (permutations.find(word) != permutations.end()) {
+            results[matches++] = word;
+            if (matches == MAXRESULTS) {
+                return matches;
             }
         }
     }
@@ -57,31 +57,101 @@ void display(std::vector<std::string>& results) {
     }
 }
 
-int main() {
-    std::vector<std::string> results(MAXRESULTS);
-    std::vector<std::string> dict(MAXDICTWORDS);
-    std::fstream dictfile;  // file containing the list of words
-    int nwords;             // number of words read from dictionary
-    std::string word;
-
-    dictfile.open("words.txt");
-    if (!dictfile) {
-        std::cout << "File not found!" << std::endl;
-        return (1);
+void testtwo(int n) {
+    vector<string> dictionary;
+    vector<string> results(MAXRESULTS);
+    switch (n) {
+        default: {
+            cout << "Bad argument" << endl;
+        } break;
+        case 1: {
+            istringstream iss("dog\ncat\nrat\neel\ntar\nart\nlee\nact\ngod\n");
+            int numResults = loadDictionary(iss, dictionary);
+            sort(dictionary.begin(), dictionary.end());
+            assert(numResults == 9 && dictionary[0] == "act" &&
+                   dictionary[1] == "art" && dictionary[numResults - 1] == "tar");
+        } break;
+        case 2: {
+            // File is empty, Checks that file is empty and loadDictionary returns 0.
+            istringstream iss("");
+            int numResults = loadDictionary(iss, dictionary);
+            assert(numResults == 0);
+        } break;
+        case 3: {
+            // Input file is larger than the dictionary size
+            istringstream iss("dog\ncat\nrat\neel\ntar\nart\nlee\nact\ngod\ntoo\nmany\nwords\n");
+            int numResults = loadDictionary(iss, dictionary);
+            sort(dictionary.begin(), dictionary.end());
+            assert(numResults == 10 && dictionary[MAXDICTWORDS - 1] == "too");
+        } break;
+        case 4: {
+            // If a certain word with repeated letter is shown in results more than once - still accept.
+            vector<string> dictionary{"one", "oone", "ne",
+                                      "e", "too"};
+            int numResults = permute("oto", dictionary,
+                                     results);
+            assert(numResults == 1 && results[0] == "too");
+        } break;
+        case 5: {
+            // No fraction of a permutation is being searched in dictionary
+            vector<string>
+                dictionary{"one", "oone", "ne",
+                           "e", "neoo", "oneo"};
+            int numResults = permute("neo", dictionary,
+                                     results);
+            assert(numResults == 1 && results[0] == "one");
+        } break;
+        case 6: {
+            // Checking that no error occurs if more than MAXRESULTS are found.
+            vector<string> dictionary{"true", "treu", "teru",
+                                      "teur", "ture", "tuer", "rtue", "rteu", "retu", "reut", "ruet",
+                                      "rute", "utre", "uter", "uetr", "uert", "urte", "uret", "etru",
+                                      "etur", "ertu", "erut", "eurt", "eutr"};
+            // All 24 permutations
+            int numResults = permute("true", dictionary,
+                                     results);
+            assert(numResults == MAXRESULTS);
+        } break;
+        case 7: {
+            // Checking one word was found, no duplicates.
+            vector<string> dictionary{"ppp"};
+            int numResults = permute("ppp", dictionary,
+                                     results);
+            assert(numResults == 1 && results[0] == "ppp");
+        } break;
+        case 8: {
+            streambuf* oldCoutStreamBuf = cout.rdbuf();
+            ostringstream strCout;
+            cout.rdbuf(strCout.rdbuf());
+            vector<string> results{"cat", "act"};
+            display(results);
+            cout.rdbuf(oldCoutStreamBuf);
+            string temp = strCout.str();
+            bool match1 = temp.find("act") != string::npos;
+            bool match2 = temp.find("cat") != string::npos;
+            assert(match1 && match2);
+        } break;
+        case 9: {
+            istringstream iss("tier\nrite\nbate\ntire\nttir\n");
+            int numWords = loadDictionary(iss, dictionary);
+            sort(dictionary.begin(), dictionary.end());
+            assert(numWords == 5 && dictionary[0] == "bate");
+            int numResults = permute("tier", dictionary,
+                                     results);
+            assert(numResults == 3 && (results[2] == "tire" ||
+                                       results[2] == "tier" || results[2] == "rite"));
+        } break;
+        case 10: {
+            vector<string> example{"kool", "moe", "dee"};
+            int numResults = permute("look", example, results);
+            assert(numResults == 1 && results[0] == "kool");
+        } break;
     }
+}
 
-    nwords = loadDictionary(dictfile, dict);
-
-    dictfile.close();
-
-    std::cout << "Please enter a string for an anagram: ";
-    std::cin >> word;
-
-    int numMatches = permute(word, dict, results);
-    if (!numMatches)
-        std::cout << "No matches found" << std::endl;
-    else
-        display(results);
-
-    return 0;
+int main() {
+    for (unsigned int i = 1; i <= 10; ++i) {
+        testtwo(i);
+    }
+    cout << "Passed" << endl;
 }
--- project_2/removeBad.cpp
+++ project_2/removeBad.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+using namespace std;
 
 void removeOdds(std::list<int>& l) {
     for (auto index = l.begin();
@@ -57,4 +58,88 @@ void removeBad(std::vector<Movie*>& l) {
             ++index;
         }
     }
+}
+
+void testone(int n) {
+    switch (n) {
+        default: {
+            assert(false);
+        } break;
+        case 1: {  // removeOdds(<list>&)
+            int a[8] = {2, 8, 5, 6, 7, 3, 4, 1};
+            list<int> x(a, a + 8);  // construct x from the array
+            assert(x.size() == 8 && x.front() == 2 && x.back() == 1);
+            removeOdds(x);
+            assert(x.size() == 4);
+            vector<int> v(x.begin(), x.end());  // construct v from x
+            sort(v.begin(), v.end());
+            int expect[4] = {2, 4, 6, 8};
+            for (int k = 0; k < 4; k++)
+                assert(v[k] == expect[k]);
+        } break;
+        case 2: {  // removeOdds(<vector>&)
+            int a[8] = {2, 8, 5, 6, 7, 3, 4, 1};
+            vector<int> x(a, a + 8);  // construct x from the array
+            assert(x.size() == 8 && x.front() == 2 && x.back() == 1);
+            removeOdds(x);
+            assert(x.size() == 4);
+            sort(x.begin(), x.end());
+            int expect[4] = {2, 4, 6, 8};
+            for (int k = 0; k < 4; k++)
+                assert(x[k] == expect[k]);
+        } break;
+        case 3: {  // removeBad(<list>&)
+            int a[8] = {85, 80, 30, 70, 20, 15, 90, 10};
+            list<Movie*> x;
+            for (int k = 0; k < 8; k++)
+                x.push_back(new Movie(a[k]));
+            assert(x.size() == 8 && x.front()->rating() == 85 &&
+                   x.back()->rating() == 10);
+            removeBad(x);
+            assert(x.size() == 4 && destroyedOnes.size() == 4);
+            vector<int> v;
+            for (list<Movie*>::iterator p = x.begin(); p !=
+                                                       x.end();
+                 p++) {
+                Movie* mp = *p;
+                v.push_back(mp->rating());
+            }
+            sort(v.begin(), v.end());
+            int expect[4] = {70, 80, 85, 90};
+            for (int k = 0; k < 4; k++)
+                assert(v[k] == expect[k]);
+            sort(destroyedOnes.begin(), destroyedOnes.end());
+            int expectGone[4] = {10, 15, 20, 30};
+            for (int k = 0; k < 4; k++)
+                assert(destroyedOnes[k] == expectGone[k]);
+        } break;
+        case 4: {  // removeBad(<vector>&)
+            destroyedOnes.clear();
+            int a[8] = {85, 80, 30, 70, 20, 15, 90, 10};
+            vector<Movie*> x;
+            for (int k = 0; k < 8; k++)
+                x.push_back(new Movie(a[k]));
+            assert(x.size() == 8 && x.front()->rating() == 85 &&
+                   x.back()->rating() == 10);
+            removeBad(x);
+            assert(x.size() == 4 && destroyedOnes.size() == 4);
+            vector<int> v;
+            for (int k = 0; k < 4; k++)
+                v.push_back(x[k]->rating());
+            sort(v.begin(), v.end());
+            int expect[4] = {70, 80, 85, 90};
+            for (int k = 0; k < 4; k++)
+                assert(v[k] == expect[k]);
+            sort(destroyedOnes.begin(), destroyedOnes.end());
+            int expectGone[4] = {10, 15, 20, 30};
+            for (int k = 0; k < 4; k++)
+                assert(destroyedOnes[k] == expectGone[k]);
+        }
+    }
+}
+int main() {
+    for (unsigned int index = 1; index <= 4; ++index) {
+        testone(index);
+    }
+    std::cout << "Passed\n";
 }
